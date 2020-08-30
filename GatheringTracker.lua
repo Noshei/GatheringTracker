c5f7dd309a2ded6c36a56b4c9ade989b61c7d2de
Noshei <Wildcat.Jason@gmail.com>
2020-08-29 20:53:33 -0600

    getting started

---
+++ GatheringTracker.lua
@@ -0,0 +1,18 @@
+GatheringTracker = LibStub("AceAddon-3.0"):NewAddon("GatheringTracker", "AceConsole-3.0", "AceEvent-3.0", "AceComm-3.0", "AceBucket-3.0")
+local AceGUI = LibStub("AceGUI-3.0")
+
+function GatheringTracker:OnInitialize()
+
+end
+
+function GatheringTracker:OnEnable()
+    
+end
+
+function GatheringTracker:OnDisable()
+
+end
+
+local frame = AceGUI:Create("Frame")
+frame:SetTitle("Gathering Tracker")
+frame:SetStatusText("This is a blank window")
